9b392eb38bd27a039d896f99168500bc53c22587
Phil Hawksworth <phil@netlify.com>
2021-05-02 12:20:25 +0100

    webhook to commit

--- src/functions/newpost.js
+++ src/functions/newpost.js
@@ -1,33 +1,89 @@
+require('dotenv').config();
 
-exports.handler = async event => {
+const slugify = require('slugify');
+const { Octokit } = require("@octokit/rest");
+const { Base64 } = require("js-base64")
 
-console.log(`Handling email content from webhook`);
-// console.log({event});
+const { 
+  GH_PERSONAL_ACCESS_TOKEN,
+  GH_USER,
+  GH_REPO
+} = process.env;
 
 
+// Authenticate with GitHub via an access token - https://github.com/settings/tokens
+const octokit = new Octokit({
+  auth: GH_PERSONAL_ACCESS_TOKEN
+});
 
-  
+
+// Get the sha of a file is it exists in the repo
+async function getSHA(path) {
+  try {
+    const { data } = await octokit.repos.getContent({
+      owner: GH_USER,
+      repo: GH_REPO,
+      path
+    });
+    console.log("RESULT: ", data.sha);
+    return data.sha;
+  } catch (err) {
+    return undefined;
+  } 
+};
+
+
+// Commit the new file or update it if it exists
+async function commitArticle(path, title, content) {
+  const sha = await getSHA(path);
+  try {
+    const { status } = await octokit.repos.createOrUpdateFileContents({
+      owner: GH_USER,
+      repo: GH_REPO,
+      path,
+      message: `Add post "${title}"`,
+      content: Base64.encode(content),
+      sha
+    });
+    return status;
+  } catch (err) {
+    console.log(`Error creating or updating: ${err}`);
+    return null;
+  }
+};
+
+
+exports.handler = async (event) => {
+
+  console.log(`Committing to GitHub`);
+
+  // get what we need from the webhook payload
   const {
     subject,
     text,
     to,
     from,
-    date,
     messageId
   } = JSON.parse(event.body)
 
-  console.log(subject, date, to.text);
-  
 
-//   console.log(to.value[0].address);
-//   console.log(from.value[0].address);
-//   console.log(date);
-//   console.log(messageId);
-//   console.log(subject);
-//   console.log(text);
+
+  const now = new Date();
+  const datestamp = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+  const slug = slugify(subject);
+  const path = `src/site/posts/${datestamp}-${slug}.md`;
+  const content = `---
+title: ${subject}
+date: ${now}
+---
+${text}
+`;
+
+  await commitArticle(path, subject, content);
 
   return {
     statusCode: 200,
-    body: `ok`,
+    body: "File added to GitHub repo",
   }
+
 }
