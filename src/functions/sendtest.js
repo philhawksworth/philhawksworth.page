eb7fba2212627e007bfdc0b697e9b56dfff4ea46
Phil Hawksworth <phil@netlify.com>
2021-05-02 13:24:22 +0100

    fine. prod URL then

--- src/functions/sendtest.js
+++ src/functions/sendtest.js
@@ -35,13 +35,11 @@ exports.handler = async event => {
     "raw": "DKIM-Signature: v=1; a=rsa-sha256; c=relaxed/relaxed; d=forwardemail.net;\r\n q=dns/txt; s=default; bh=fdkeB/A0FkbVP2k4J4pNPoeWH6vqBm9+b0C3OY87Cw8=;\r\n h=from:subject:date:message-id:to:mime-version:content-type:content-transfer-encoding;\r\n b=KJZp0q0u/cQhcjwilKMainmlystwHgCZ7/ncK1uBmmdGoaXlQcMHsfenLyn/uribhMVrdfWw6\r\n YhQ5AIOAGoft/fwpGhl3zP1b5qrPwYu0kLMPr2MSwkLo0YVdbHB6xF+VGeg2vaduJk6CipXjMW7\r\n Mlohmvjw0m1tnN6dAYGOkwQ=\r\nMessage-ID: <123.abc@test>\r\nDate: Thu, 9 Nov 2000 10:44:00 -0800 (PST)\r\nTo: blogpost@secret.fyi\r\nFrom: Test <phil@netlify.com>\r\nSubject: testing webhooks\r\nMime-Version: 1.0\r\nContent-Type: text/plain; charset=us-ascii\r\nContent-Transfer-Encoding: 7bit\r\n\r\nTest\r\n"
   };
   
-  const rootURL = process.env.DEPLOY_URL ? process.env.DEPLOY_URL : "https://localhost:8888";
+  const rootURL = process.env.URL ? process.env.URL : "https://localhost:8888";
 
   console.log(`test will post to: ${rootURL}/api/newpost`);
 
-  
-  console.log(process.env);
-  
+
  
   request.post({
     headers: {
